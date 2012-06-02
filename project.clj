c111fd4645a0f459f2ae67dd158a53f19df12f96
nipra <nipra@lambda.(none)>
2012-06-02 15:07:37 +0530

    More dependencies.

--- project.clj
+++ project.clj
@@ -6,25 +6,115 @@
   ;; http://stackoverflow.com/questions/7511789/clojure-lein-how-do-i-include-a-java-file-in-my-project
   ;; http://alexott.net/en/clojure/ClojureLein.html
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.mongodb/mongo-java-driver "2.6.3"]
+
                  [postgresql/postgresql "9.0-801.jdbc4"]
                  [mysql/mysql-connector-java "5.1.18"]
 
                  [org.apache.lucene/lucene-core "3.6.0"]
                  [org.apache.lucene/lucene-queries "3.6.0"]
                  [clucy "0.3.0"]
+
                  ;; Core interfaces/classes, external parser libs, GUI, CLI
                  [org.apache.tika/tika-app "1.1"]
 
                  [org.apache.solr/solr-core "3.6.0"]
                  [org.apache.solr/solr-solrj "3.6.0"]
-                 
-                 [javax.mail/mail "1.4.1"]
-                 [com.rabbitmq/amqp-client "2.7.1"]
+
                  [com.indextank/indextank-java "1.0.9"]
-                 ;; [javax.units/jsr108 "0.01"] ; http://jsr-108.sourceforge.net/javadoc/index.html
+
+                 ;; http://jsr-108.sourceforge.net/javadoc/index.html
+                 ;; [javax.units/jsr108 "0.01"] 
 
                  [org.clojars.tavisrudd/redis-clojure "1.3.1"]
+
+                 ;; A tiny library wrapping language-detect that can be used
+                 ;; to determine the language of a particular piece of text.
+                 ;; https://github.com/dakrone/cld
+                 [cld "0.1.0"]
+
+                 ;; Quarzite is a powerful Clojure scheduling library built on
+                 ;; top the Quartz Scheduler.
+                 [clojurewerkz/quartzite "1.0.0-rc5"]
+
+                 [com.rabbitmq/amqp-client "2.7.1"]
+                 ;; Langohr is a Clojure wrapper around the RabbitMQ Java client
+                 [com.novemberain/langohr "1.0.0-beta2"]
+
+                 ;; [org.mongodb/mongo-java-driver "2.6.3"]
+                 ;; Monger is an idiomatic Clojure MongoDB driver for a more
+                 ;; civilized age.
+                 [com.novemberain/monger "1.0.0-beta7"]
+                 
+                 ;; http://neo4j.org/download/
+                 [org.neo4j/neo4j "1.7.1"]
+                 ;; A Neo4j library for Clojure
+                 [clojure-neo4j "0.3.0-SNAPSHOT"]
+                 ;; Clojure wrapper for Neo4j, a graph database.
+                 ;; https://github.com/wagjo/borneo
+                 [borneo "0.3.0"]
+                 ;; https://github.com/scusack/neo4j-clj
+                 [neo4j-clj "0.0.2-SNAPSHOT"]
+                 ;; Neocons, a Clojure client for the Neo4J REST API
+                 [clojurewerkz/neocons "1.0.0-beta4"]
+
+                 ;; Welle is an expressive Clojure client for Riak with
+                 ;; batteries included.
+                 [com.novemberain/welle "1.0.0"]
+
+                 ;; Clojure OAuth library
+                 ;; https://github.com/r0man/oauth-clj
+                 [oauth-clj "0.0.5"]
+                 ;; OAuth Consumer support for Clojure
+                 ;; https://github.com/mattrepl/clj-oauth
+                 [clj-oauth "1.2.10"]
+
+                 ;; A simple and flexible library for shelling out in Clojure
+                 ;; https://github.com/Raynes/conch
+                 [conch "0.2.1"]
+
+                 [javax.mail/mail "1.4.1"]
+                 ;; Postal is a library for constructing and sending
+                 ;; RFC822-compliant Internet email messages.
+                 ;; https://github.com/drewr/postal
+                 [com.draines/postal "1.7.1"]
+                 ;; Mailer is an ActionMailer-inspired mailer library for Clojure.
+                 [clojurewerkz/mailer "1.0.0-alpha3"]
+
+                 ;; Clojure HTTP library using the Apache HttpClient.
+                 ;; https://github.com/rnewman/clj-apache-http
+                 [com.twinql.clojure/clj-apache-http "2.3.1"]
+                 ;; A Clojure HTTP library wrapping the Apache
+                 ;; HttpComponents client.
+                 ;; https://github.com/dakrone/clj-http
+                 [clj-http "0.4.2"]
+                 ;; http.async.client is based on Asynchronous Http Client
+                 ;; for Java.
+                 ;; https://github.com/neotyk/http.async.client
+                 [http.async.client "0.4.5"]
+
+                 ;; Async io interface to all the twitter APIs
+                 ;; https://github.com/adamwynne/twitter-api
+                 [twitter-api "0.6.10"]
+
+                 ;; Clojure JSON and JSON SMILE (binary json format)
+                 ;; encoding/decoding
+                 ;; https://github.com/dakrone/cheshire
+                 [cheshire "4.0.0"]
+
+                 ;; Enhanced try and throw for Clojure leveraging Clojure's
+                 ;; capabilities 
+                 ;; https://github.com/scgilardi/slingshot
+                 [slingshot "0.10.2"]
+
+                 ;; A clojure wrapper for Reddit API
+                 ;; https://github.com/sunng87/reddit.clj
+                 [reddit.clj "0.3.3"]
+
+                 ;; Latest versions from
+                 ;; http://mvnrepository.com/artifact/org.apache.httpcomponents
+                 [org.apache.httpcomponents/httpmime "4.1.3"]
+                 [org.apache.httpcomponents/httpclient "4.1.3"]
+                 [org.apache.httpcomponents/httpcore "4.1.3"]
 
                  [org.clojure/algo.generic "0.1.0"]
                  [org.clojure/algo.monads "0.1.0"]
@@ -60,8 +150,7 @@
                  [org.clojure/tools.macro "0.1.1"]
                  [org.clojure/tools.namespace "0.1.2"]
                  [org.clojure/tools.nrepl "0.0.5"]
-                 [org.clojure/tools.trace "0.7.3"]
-                 ]
+                 [org.clojure/tools.trace "0.7.3"]]
 
   :dev-dependencies [[swank-clojure "1.4.0"]]
   :repositories {
@@ -69,4 +158,5 @@
                  "sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"
                  ;; "sonatype-oss-snapshots" "https://oss.sonatype.org/content/repositories/snapshots"
                  })
+
 
