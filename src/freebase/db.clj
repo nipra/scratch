c206077645a3cb93aae7bf9dc8147f034182cbdb
Nikhil Prabhakar <prabhakar.nikhil@gmail.com>
2012-07-20 00:00:11 +0530

    Connection pool example. Some freebase queries.

--- src/freebase/db.clj
+++ src/freebase/db.clj
@@ -2,7 +2,7 @@
   (:require [utils.postgres :as pg])
   (:use [korma.db]
         [korma.core])
-  (:require [clojure.java [jdbc :as sql]]
+  (:require [clojure.java [jdbc :as jdbc]]
             [clojure.pprint :as p])
   (:import [java.sql Connection DriverManager ResultSet SQLException
             Statement ResultSetMetaData]))
@@ -17,10 +17,30 @@
                       :user "postgres"
                       :password "postgres"}))
 
-
-
 (defentity articles)
 
 (comment
   (select (database articles wex)
           (limit 1)))
+
+
+;;;
+(def db-conn
+  (connection-pool {:classname "org.postgresql.Driver"
+                    :subprotocol "postgresql"
+                    :subname "//localhost/wikipedia"
+                    :user "postgres"
+                    :password "postgres"
+                    :db "wikipedia"}))
+
+(comment
+  (jdbc/with-connection db-conn
+    (jdbc/with-query-results rows
+      ["select count(*) from articles"]
+      (first rows)))
+  (jdbc/with-connection db-conn
+    (jdbc/with-query-results rows
+      ["select name from articles
+        where to_tsvector(name) @@ to_tsquery('lost')
+        limit 10"]
+      (doall rows))))
