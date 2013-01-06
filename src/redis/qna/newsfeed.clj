7af91ff8f4788112f767219d8035c5b947c84d7e
Nikhil Prabhakar <prabhakar.nikhil@gmail.com>
2013-01-06 21:02:03 +0530

    Adding relevant link(s) for implementing a newsfeed in Redis.

---
+++ src/redis/qna/newsfeed.clj
@@ -0,0 +1,9 @@
+(ns redis.qna.newsfeed)
+
+(comment
+  ["http://rfw.posterous.com/a-redis-powered-newsfeed-implementation"
+   "nosql.mypopescu.com/post/522925057/redis-powered-facebook-like-newsfeeds"
+   "http://www.quora.com/What-are-best-practices-for-building-something-like-a-News-Feed"
+   "http://blog.waxman.me/how-to-build-a-fast-news-feed-in-redis"])
+
+;;; TODO: IMPLEMENT!!!
