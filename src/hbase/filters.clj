020c9289bb98d6c33b919df6e8ac3259c8c05721
Nikhil Prabhakar <nprabhak@unmac.local>
2013-01-24 19:02:15 +0530

    Added value-filter and single-column-value-filter.

--- src/hbase/filters.clj
+++ src/hbase/filters.clj
@@ -2,8 +2,6 @@
   (:import (org.apache.hadoop.hbase.filter ValueFilter
                                            CompareFilter
                                            CompareFilter$CompareOp
-                                           WritableByteArrayComparable
-                                           BinaryComparator
                                            SingleColumnValueFilter
                                            PrefixFilter
                                            QualifierFilter
@@ -13,7 +11,41 @@
                                            RegexStringComparator
                                            ColumnRangeFilter
                                            ColumnPrefixFilter)
+           (org.apache.hadoop.hbase.filter WritableByteArrayComparable
+                                           BinaryComparator
+                                           BinaryPrefixComparator
+                                           BitComparator
+                                           BitComparator$BitwiseOp)
            (org.apache.hadoop.hbase.util Bytes)))
+
+(def eq CompareFilter$CompareOp/EQUAL)
+(def gt CompareFilter$CompareOp/GREATER)
+(def gte CompareFilter$CompareOp/GREATER_OR_EQUAL)
+(def lt CompareFilter$CompareOp/LESS)
+(def lte CompareFilter$CompareOp/LESS_OR_EQUAL)
+(def ne CompareFilter$CompareOp/NOT_EQUAL)
+
+(declare make-bit-comparator)
+(def binary-and (make-bit-comparator BitComparator$BitwiseOp/AND))
+(def binary-or (make-bit-comparator BitComparator$BitwiseOp/OR))
+(def binary-xor (make-bit-comparator BitComparator$BitwiseOp/XOR))
+
+(defn binary-comparator
+  [value]
+  (BinaryComparator. (Bytes/toBytes value)))
+
+(defn binary-prefix-comparator
+  [value]
+  (BinaryPrefixComparator. (Bytes/toBytes value)))
+
+(defn bit-comparator
+  [value bitwise-op]
+  (BitComparator. (Bytes/toBytes value) bitwise-op))
+
+(defn make-bit-comparator
+  [bitwise-op]
+  (fn [value]
+    (bit-comparator value bitwise-op)))
 
 (defn column-filter
   [column-family qualifier]
@@ -51,3 +83,22 @@
 (defn row-prefix-filter
   [prefix]
   (PrefixFilter. (Bytes/toBytes prefix)))
+
+;;; Value filters
+(defn value-filter
+  [value & {:keys [compare-op comparator-fn]
+            :or {compare-op eq
+                 comparator-fn binary-comparator}}]
+  (ValueFilter. compare-op (comparator-fn value)))
+
+(defn column-value-filter
+  "NOTE: When using this filter on a Scan with specified inputs, the column to 
+   be tested should also be added as input (otherwise the filter will regard the
+   column as missing)."
+  [column-family qualifier value & {:keys [compare-op comparator-fn]
+                                    :or {compare-op eq
+                                         comparator-fn binary-comparator}}]
+  (SingleColumnValueFilter. (Bytes/toBytes column-family)
+                            (Bytes/toBytes qualifier)
+                            compare-op
+                            comparator-fn))
