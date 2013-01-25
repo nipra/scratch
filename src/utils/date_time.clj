8096c6bd268454ba0aac91aaf561f16906bf0335
Nikhil Prabhakar <nprabhak@unmac.local>
2013-01-25 13:37:23 +0530

    Date time utils.

---
+++ src/utils/date_time.clj
@@ -0,0 +1,30 @@
+(ns utils.date-time
+  (:import (org.joda.time Period Seconds))
+  (:import (org.joda.time.format PeriodFormatterBuilder PeriodFormatter))
+  (:require (clj-time [core :as ctc]
+                      [format :as ctf]
+                      [coerce :as ctco]
+                      [local :as ctl])))
+
+;;; FIXME: Incomplete
+(defn readable-secs
+  [secs]
+  (let [period (.toPeriod (Seconds/seconds secs))
+        period-formatter (.toFormatter (doto (PeriodFormatterBuilder.)
+                                         (.appendDays)
+                                         (.appendSuffix " day" "days")))]
+    (with-out-str (println (.print period-formatter period)))))
+
+(defn yyyymmdd-range
+  [start stop]
+  (let [formatter (ctf/formatter "yyyyMMdd")
+        start-date (ctf/parse formatter start)
+        stop-date (ctf/parse formatter stop)]
+    (loop [dates [start]
+           prev-date start-date
+           next-date (ctc/plus prev-date (ctc/days 1))]
+      (if (ctc/within? (ctc/interval start-date stop-date) next-date)
+        (recur (conj dates (ctf/unparse formatter next-date))
+               next-date
+               (ctc/plus next-date (ctc/days 1)))
+        (conj dates stop)))))
