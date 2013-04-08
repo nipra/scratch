0c3d7fe6876caed3a85a0b48ba463abc00383655
Nikhil Prabhakar <nprabhak@unmac.local>
2013-04-08 17:33:00 +0530

    Added url-decode and is-encoded?

--- src/utils/net.clj
+++ src/utils/net.clj
@@ -1,9 +1,15 @@
 (ns utils.net
-  (:import (java.net URLEncoder)))
+  (:import (java.net URLEncoder URLDecoder)))
 
 (defn url-encode
-  ([url]
-     (url-encode url "UTF-8"))
+  [url & [encoding-scheme]]
+  (URLEncoder/encode url (or encoding-scheme "UTF-8")))
 
-  ([url encoding-scheme]
-     (URLEncoder/encode url encoding-scheme)))
+(defn url-decode
+  [url & [encoding-scheme]]
+  (URLDecoder/decode url (or encoding-scheme "UTF-8")))
+
+(defn is-encoded?
+  [url & [encoding-scheme]]
+  (not= (url-decode url encoding-scheme)
+        url))
