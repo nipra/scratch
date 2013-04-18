e3f5e86ad8c113496dd4273aba16b8bb993d3339
Nikhil Prabhakar <nprabhak@unmac.local>
2013-04-18 16:44:23 +0530

    Added kv-pairs->map.

--- src/utils/seq.clj
+++ src/utils/seq.clj
@@ -1,6 +1,7 @@
 (ns ^{:doc "Original sources for code written here:
    1. https://github.com/overtone/overtone/blob/master/src/overtone/helpers/seq.clj
-   2. https://github.com/flatland/useful/blob/develop/src/flatland/useful/seq.clj"}
+   2. https://github.com/flatland/useful/blob/develop/src/flatland/useful/seq.clj
+   3. /me"}
   utils.seq
   (:import (java.util Collection)))
 
@@ -19,3 +20,13 @@
   "Check if `val' exists in `coll'."
   [val coll]
   (some (partial = val) coll))
+
+;;; 
+(defn kv-pairs->map
+  "input => [[:a 1] [:b 2] [:a 2] [:c 3]]
+   output => {:a [1 2] :b [2] :c [3]}"
+  [kv-pairs]
+  (reduce (fn [result x]
+            (update-in result [(first x)] conj (second x)))
+          {}
+          kv-pairs))
